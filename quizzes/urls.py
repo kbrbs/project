18e66fc929fdc4b988e25c3be381e35a0b5bcfdd
Katrina <katrinamicaellabarbosa@gmail.com>
2025-10-30 16:55:09 +0800

    quiz ui

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.quiz_list, name='quiz_list'),
     path('<int:pk>/', views.quiz_detail, name='quiz_detail'),
     path('api/<int:pk>/', views.quiz_api_detail, name='quiz_api_detail'),
+    path('submit/<int:pk>/', views.quiz_submit, name='quiz_submit'),
 ]
