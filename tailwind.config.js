fd366eb08dab165f9c62442063328b91f2c3252d
Katrina <katrinamicaellabarbosa@gmail.com>
2025-11-19 04:32:35 +0800

    FESTIVAL TOUR CHANGES

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './core/templates/**/*.html',
+    './quizzes/templates/**/*.html',
+    './games/templates/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
